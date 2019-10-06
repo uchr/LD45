175b5a4fac7b805fb345d1e4511f0e6aa7c5fb22
Anton Tarasov <uchiro.me@gmail.com>
2019-10-06 16:58:35 +0700

    Remove some buildings

---
+++ LD45-Project/Assets/Scripts/Graveyard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graveyard : MonoBehaviour {
+    private List<Vector3> positions;
+
+    private void Start() {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("House");
+    }
+
+    private void Update() {
+        
+    }
+}
